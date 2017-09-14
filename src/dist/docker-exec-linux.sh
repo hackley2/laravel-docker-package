e4ef73dbae2ef016b3108363740260fd2aca89e3
Andrew Hackley <hackley2@gmail.com>
2017-09-14 07:25:48 -0500

    updated linux's exec.sh file with better documentation

--- src/dist/docker-exec-linux.sh
+++ src/dist/docker-exec-linux.sh
@@ -3,10 +3,13 @@
 # Runs an exec command on the specified docker container and the specified
 #    command as a user with the same permissions as the host machine's
 #    permissions
+# You can optionally specify additional docker parameters before the
+#    docker container name if needed
 #
 # Examples of usage:
 #    ./docker-exec-linux.sh <docker-container-name> bash
 #    ./docker-exec-linux.sh <docker-container-name> composer update
+#    ./docker-exec-linux.sh --env ENV_VAR=some-thing <docker-container-name> bash
 #
 
 docker exec -ti -u $(id -u):$(id -g) "$@"
